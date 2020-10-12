33d693f250295c8f1a2825a45b33213960c59e6d
ZychGrzegorz <grzegorzzych95@gmail.com>
2020-10-12 13:18:22 +0200

    Updates

---
+++ precache-manifest.e9c5bfa76ebdfca8d5a9b009c831c2ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50f2f910db4011cf2542084d207c547c",
+    "url": "/weatherapp-ts-react-redux/index.html"
+  },
+  {
+    "revision": "2cf3c217c75ed76a7e97",
+    "url": "/weatherapp-ts-react-redux/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "03285afbe1810ee1c7af",
+    "url": "/weatherapp-ts-react-redux/static/js/2.971650e6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/weatherapp-ts-react-redux/static/js/2.971650e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cf3c217c75ed76a7e97",
+    "url": "/weatherapp-ts-react-redux/static/js/main.b1f85ae7.chunk.js"
+  },
+  {
+    "revision": "5eb772dca8aad7e9ee59",
+    "url": "/weatherapp-ts-react-redux/static/js/runtime-main.3e3c8a1c.js"
+  }
+]);
